8ec4d401a51f19cb6b9568227ff52ead57141f2c
lu4ckex <lu4ckex@outlook.com>
2024-08-12 20:42:29 -0300

    Essa calculadora faz calculos de números inteiros e decimais.

---
+++ Calculadora.py
@@ -0,0 +1,8 @@
+print('--------------- Programa da Calculadora V. 1.0 ---------------')
+print()
+
+numero1 = float(input('Digite o primeiro valor: '))  # Aceita números inteiros e decimais.
+numero2 = float(input('Digite o segundo valor: '))
+soma = numero1 + numero2
+
+print('A soma dos dois números é:', soma)
