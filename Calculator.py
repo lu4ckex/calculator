34894160515e7d9f78899af35c37d48a8a390993
lu4ckex <lu4ckex@outlook.com>
2024-08-12 21:40:42 -0300

    Accepts integers and decimals

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+print('--------------- Calculator Program v1.0 ---------------')
+print()
+
+number1 = float(input('Enter the first value: '))  # Accepts integers and decimals.
+number2 = float(input('Enter the second value: '))
+soma = number1 + number2
+
+print('The sum of the two numbers is: ',soma)
